9e63b4004208869423663aa758a0be35eacf8c25
admin <fuxiuzhan>
2021-12-04 12:35:46 +0800

    init

---
+++ src/main/java/com/fxz/fuled/service/annotation/EnableFuledBoot.java
@@ -0,0 +1,19 @@
+package com.fxz.fuled.service.annotation;
+
+
+import com.fxz.fuled.simple.cache.EnableSimpleCache;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.annotation.*;
+
+/**
+ * @author fxz
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@SpringBootApplication
+@EnableSimpleCache
+public @interface EnableFuledBoot {
+}
